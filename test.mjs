ea95bf4d6fc3cf77e78385644d53f15c2633deb5
Li Ye <liye@xd.com>
2024-01-09 14:52:37 +0800

    feat: caculate pxx stddev and histogram

--- test.mjs
+++ test.mjs
@@ -1,6 +1,8 @@
 import Play from "@leancloud/play";
 import yargs from "yargs/yargs";
 import { hideBin } from "yargs/helpers";
+import { stdev, percentile } from "stats-lite";
+import Histogram from "histogram-simple";
 
 // 使用 yargs 解析命令行参数
 const argv = yargs(hideBin(process.argv)).option("tickrate", {
@@ -16,6 +18,7 @@ const configs = {
   appKey: "TMkbH8yfIO2APRioG2joeety", // 游戏的 Client Token
   playServer: "https://oge8zdrp.lc-cn-n1-shared.com", // 游戏的 API 域名
   gameVersion: "0.0.1", // 设置游戏版本号，选填，默认 0.0.1，不同版本的玩家不会匹配到同一个房间
+  // ssl: false,
 };
 const player1 = new Client({
   ...configs,
@@ -39,28 +42,60 @@ player1.on(Event.CUSTOM_EVENT, ({ eventId, eventData, senderId }) => {
   });
 });
 
+const MAX_COUNT = 2500;
+
 let i = 0;
+const RTTs = [];
 let received = 0;
 let totalTime = 0;
 const ts = {};
 player2.on(Event.CUSTOM_EVENT, ({ eventData }) => {
   if (ts[eventData.i]) {
-    received++;
-    const rtt = Date.now() - ts[eventData.i];
-    totalTime += rtt;
+    const rtt = (now() - ts[eventData.i]) / 1000;
+    RTTs.push(rtt);
 
-    const lostRate = Math.floor((1 - received / eventData.i) * 10000) / 100;
-    const meanRTT = Math.round(totalTime / received);
-    console.log(
-      `[${eventData.i}] RTT: ${rtt} LOST: ${lostRate}% MEAN RTT: ${meanRTT}`
+    const lossRate = Math.floor((1 - RTTs.length / eventData.i) * 10000) / 100;
+    process.stdout.write(
+      `[${eventData.i}/${MAX_COUNT}] RTT: ${rtt} LOST: ${lossRate}%\r`
     );
 
     delete ts[eventData.i];
+
+    if (eventData.i === MAX_COUNT) {
+      console.log(
+        `RTT P99/95/75/50: ${[99, 95, 75, 50].map((percent) =>
+          percentile(RTTs, percent / 100)
+        )} stddev: ${stdev(RTTs)}`
+      );
+      const histogram = Histogram(
+        RTTs.sort((a, b) => a - b).slice(0, Math.floor(0.96 * RTTs.length)),
+        20
+      );
+      console.log(
+        `RTT histogram: `,
+        histogram.resultCounts,
+        `
+${histogram.toString(100)}`
+      );
+      console.log(
+        "0-999:",
+        JSON.stringify(
+          Object.values(Histogram(RTTs, 1000, 0, 1000).resultCounts)
+        )
+      );
+
+      console.log(`LOSS: ${lossRate}%`);
+      process.exit(0);
+    }
   }
 });
-setInterval(() => {
+
+const intervalId = setInterval(() => {
+  if (i >= MAX_COUNT) {
+    return clearInterval(intervalId);
+  }
   i++;
-  ts[i] = Date.now();
+  ts[i] = now();
   player2.sendEvent(
     0,
     { i },
@@ -69,3 +104,8 @@ setInterval(() => {
     }
   );
 }, Math.round(1000 / argv.tickrate));
+
+const now = () => {
+  const hrTime = process.hrtime();
+  return hrTime[0] * 1000000 + Math.round(hrTime[1] / 1000);
+};
